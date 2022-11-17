1b2a89da6925cbd1cbdc7432f304eba2ee58dc47
JunBeum-Cho <junbeumcho@gmail.com>
2022-11-18 00:52:51 +0900

    package name changed

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hola-mac",
+    name="hola-dev",
     version="0.0.1",
     author="JunBeum Cho",
     author_email="ahengksk@gmail.com",
     description="Common packages installer on MAC",
     url = 'https://github.com/JunBeum-Cho/hola-mac',
     download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
-    entry_points={"console_scripts": ["hola-mac=src.__main__:main"]},
+    entry_points={"console_scripts": ["hola-dev=src.__main__:main"]},
     keywords = ['mac', 'package installers'],
     install_requires=required,
     long_description=long_description,
