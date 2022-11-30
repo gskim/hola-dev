8ded4d3bd7b655eccc2e06f3530facebb01618c6
JunBeum-Cho <junbeumcho@gmail.com>
2022-11-30 16:17:11 +0900

    version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hola-dev",
-    version="0.0.4",
+    version="0.0.5",
     author="JunBeum Cho",
     author_email="ahengksk@gmail.com",
     description="Common packages installer on MAC",
